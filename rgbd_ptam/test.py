926b127949814e7d33e35bdd7d4341513b8ddd0c
Panagiotis Petropoulakis <petp@atcremers92.mlr.in.tum.de>
2024-07-27 12:29:18 +0200

    working-on-tests

--- rgbd_ptam/test.py
+++ rgbd_ptam/test.py
@@ -10,78 +10,7 @@ from components import Camera, RGBDFrame, Measurement
 from params import Params
 from feature import ImageFeature
 from covisibility import CovisibilityGraph
-from optimization import BundleAdjustmentScaleAware
-
-
-
-def optimize(args, frame_1, frame_2, matched_measurements_1, matched_measurements_2, class_id=0, max_iterations=10, gt_pose=None):
-    optimizer = BundleAdjustmentScaleAware(args=args)
-    optimizer.clear()
-
-    # Poses #
-    optimizer.add_pose(0, frame_1.pose, frame_1.cam, fixed=True)
-    optimizer.add_pose(1, frame_2.pose, frame_2.cam, fixed=False)
-    
-    # Scales #
-    optimizer.add_scale(2, frame_1.scale_aware_frame.scales[class_id], fixed=True)
-    optimizer.add_scale(3, frame_1.scale_aware_frame.scales[class_id], fixed=True)
-
-
-    # Scales edges network #
-    information_1 = np.identity(1)
-    information_2 = np.identity(1)
-    if args.use_uncertainties:
-        information_1 *= 1. / frame_1.scale_aware_frame.scales_uncertainty[class_id]
-        information_2 *= 1. / frame_2.scale_aware_frame.scales_uncertainty[class_id]
-    optimizer.add_scale_edge(4, scale_id=2, meas=frame_1.scale_aware_frame.scales[class_id],
-                                  information=information_1)
-    optimizer.add_scale_edge(5, scale_id=3, meas=frame_2.scale_aware_frame.scales[class_id],
-                                  information=information_2)
-
-
-    index_point = 0
-    index_general = 0
-    points_start = 6
-    depth_scale_consistenct_start = 10000
-    camera_start = 50000
-
-    # Points frame 1 #
-    for meas in matched_measurements_1:  
-        pt = meas.mappoint
-
-        xy = meas.xy 
-
-        # 3D Landmark #
-        optimizer.add_point(index_point + points_start, pt.position, fixed=True)
-        
-        optimizer.add_camera_edge(index_general + camera_start, point_id=index_point + points_start, pose_id=0, meas=meas.xy)
-
-        
-        information = np.identity(1)
-        if args.use_uncertainties:
-            information *= 1. / m.covariance_canonical_measurement
-        optimizer.add_depth_scale_consistency_edge(index_general + depth_scale_consistenct_start, point_id=index_point + points_start, pose_id=0, \
-                                                   scale_id=2, meas=meas.canonical_measurement, information=information)
-        index_general += 1
-        index_point += 1
-    
-    # Frame 2 # 
-    index_point = 0
-    for meas in matched_measurements_2:  
-        pt = meas.mappoint
-
-        optimizer.add_camera_edge(index_general + camera_start, point_id=index_point + points_start, pose_id=1, meas=meas.xy)
-
-        information = np.identity(1)
-        if args.use_uncertainties:
-            information *= 1. / m.covariance_canonical_measurement
-        optimizer.add_depth_scale_consistency_edge(index_general + depth_scale_consistenct_start, point_id=index_point + points_start, pose_id=1, \
-                                                   scale_id=3, meas=meas.canonical_measurement, information=information)
-        index_general += 1
-        index_point += 1
-     
-
-    optimizer.optimize(max_iterations)
+from utils_tests import draw_keypoints, test_1
 
 
 def read_args(parser):
@@ -89,7 +18,7 @@ def read_args(parser):
     parser.add_argument('--debug', action='store_true', help='debug slam')
     parser.add_argument('--dataset', type=str, help='dataset (TUM/ICL-NUIM)', default='scannet')
     parser.add_argument('--path', type=str, help='dataset path',
-                        default='usr/stud/petp/storage/user/petp/datasets/scannet/data_converted')
+                        default='/usr/stud/petp/storage/user/petp/datasets/scannet/data_converted')
     parser.add_argument('--scale_aware', action='store_true', default=False, help='Scale-Aware slam loss enable')
     parser.add_argument('--network_depth', action='store_true', default=False,
                         help='Use ground truth depth or network depth')
@@ -103,17 +32,18 @@ def read_args(parser):
     parser.add_argument('--total', type=int, default=None, help='Total number of frame')
     parser.add_argument('--exp_name', type=str, default='exp_1', help='Experiment name')
     parser.add_argument('--threshold_camera', type=float, default=5.991, help='Threshold for huber loss camera')
-    parser.add_argument('--weight_camera', type=float, default=2, help='Weight for camera loss') # 1
+    parser.add_argument('--weight_camera', type=float, default=1, help='Weight for camera loss') # 1
     parser.add_argument('--threshold_depth_consistency', type=float, default=0.0025,
                         help='Threshold for huber loss depth consistency')
-    parser.add_argument('--weight_depth_consistency', type=float, default=1, help='Weight for depth consistency loss') # 0.5
+    parser.add_argument('--weight_depth_consistency', type=float, default=0, help='Weight for depth consistency loss') # 0.5
     parser.add_argument('--threshold_scale', type=float, default=0.0025, help='Threshold for huber loss scale')
-    parser.add_argument('--weight_scale', type=float, default=2, help='Weight for scale loss') # 0.5
+    parser.add_argument('--weight_scale', type=float, default=0, help='Weight for scale loss') # 0.5
     parser.add_argument('--scene', type=str, default='scene0655_01')
     parser.add_argument('--split', type=str, default='valid')
 
     args = parser.parse_args()
     return args
+
 
 def get_frame(idx, mask=False, class_id=0):
     time_start = time.time()
@@ -128,7 +58,8 @@ def get_frame(idx, mask=False, class_id=0):
     
     feature = ImageFeature(dataset.rgb[idx], params)
     feature.extract()  # Detect keypoints and descriptors of image
-    if mask:
+    if mask and False:
+        #feature.filter_features_to_class(sem_mask, 0, draw=True)
         feature.filter_features_to_class(sem_mask, 0, draw=False)
            
     frame = RGBDFrame(idx, g2o.Isometry3d(), feature, depth, cam, timestamp=timestamp,
@@ -138,10 +69,12 @@ def get_frame(idx, mask=False, class_id=0):
 
     return frame, gt_pose, sem_mask
 
+
 def normalize_angle(angle):
     return (angle + 180) % 360 - 180
 
-def add_noise(pose, rot_noise=2, trans_noise=0.1):
+
+def add_noise(pose, rot_noise=5, trans_noise=0.5):
     transformation = pose.matrix()
     rot = transformation[:3, :3]
     trans = transformation[:3, 3]
@@ -152,11 +85,11 @@ def add_noise(pose, rot_noise=2, trans_noise=0.1):
     euler = normalize_angle(euler)
     
     trans += trans_noise 
-    
     rot = R.from_euler('xyz', euler)
     quat = rot.as_quat()
     noisy_pose = g2o.Isometry3d(g2o.Quaternion(quat), trans)
     return noisy_pose
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -181,18 +114,39 @@ if __name__ == "__main__":
         
     # Frames #
     frame_1, pose_w_c1, sem_mask_1 = get_frame(0)
-    frame_2, pose_w_c2, sem_mask_2 = get_frame(3, mask=True, class_id=0)
+    frame_2, pose_w_c2, sem_mask_2 = get_frame(2, mask=True, class_id=0)
 
     pose_c1_c2 = pose_w_c1.inverse() * pose_w_c2
-    pose_c1_c2_noise = add_noise(pose_c1_c2)
-    frame_2.update_pose(pose_c1_c2_noise)
-    
+    pose_c2_c1 = pose_c1_c2.inverse()
+    pose_c2_c1_noise = add_noise(pose_c2_c1)
+
     frame_1 = frame_1.to_keyframe()
     frame_2 = frame_2.to_keyframe()
 
-    mappoints_1, measurements_1 = frame_1.cloudify()  
+    frame_1.update_pose(pose_c2_c1_noise) 
+    mappoints_1, measurements_1 = frame_1.cloudify() 
+    frame_1.update_pose(g2o.Isometry3d()) 
+    test_1(args, frame_1, measurements_1, class_id=0, max_iterations=100, gt_pose=pose_c2_c1_noise)
+    
+    
 
+
+    # c1 -> w * w -> c2 == c1 -> c2 #
+
+
+    """
+    #frame_2.update_pose(pose_c2_c1) # To find good correspondances 
     matched_measurements_2, matched_ids = frame_2.match_mappoints_and_get_ids(mappoints_1, Measurement.Source.TRACKING)
     matched_measurements_1 = np.asarray(measurements_1)[matched_ids]
-   
-    optimize(args, frame_1, frame_2, matched_measurements_1, matched_measurements_2, class_id=0, max_iterations=10, gt_pose=pose_c1_c2)
+    assert len(matched_measurements_1) == len(matched_measurements_2) 
+
+    keypoints_1 = [[cv2.KeyPoint(x=int(meas.xy[0]), y=int(meas.xy[1]), size=1) for meas in matched_measurements_1][15]]    
+    #draw_keypoints(frame_1.rgb.image, keypoints_1)
+    keypoints_2 = [[cv2.KeyPoint(x=int(meas.xy[0]), y=int(meas.xy[1]), size=1) for meas in matched_measurements_2][15]]
+    #draw_keypoints(frame_2.rgb.image, keypoints_2)
+    
+    #frame_2.update_pose(pose_c2_c1_noise)
+    #optimize(args, frame_1, frame_2, matched_measurements_1, matched_measurements_2, class_id=0, max_iterations=0, gt_pose=pose_c2_c1)
+    
+    #optimize(args, frame_1, frame_2, matched_measurements_1, matched_measurements_2, class_id=0, max_iterations=0, gt_pose=frame_1.pose)
+    """
---
+++ rgbd_ptam/utils_tests.py
@@ -0,0 +1,192 @@
+import g2o
+import cv2
+import numpy as np
+
+from optimization import BundleAdjustmentScaleAware
+
+def rpe(gt_pose, pose):
+    gt_pose_mat = gt_pose.matrix()
+    pose_mat = pose.matrix()
+
+    # c2 -> c1: c1 -> c2
+    # pose_mat: c2 -> c1 
+    transformation_error = np.linalg.inv(gt_pose_mat) @ pose_mat
+    
+    return np.linalg.norm(transformation_error[:3, 3])
+
+def get_bad_measurements(optimizer, args):
+    bad_measurements = set()
+    for edge in optimizer.active_edges():
+        #print(edge.chi2())
+        if isinstance(edge, g2o.EdgeCustomCamera):
+            if edge.chi2() > args.threshold_camera:
+                bad_measurements.add(edge.id())
+                #print(edge.error())
+                #print(edge.measurement())
+                #print(edge.initial_estimate)
+                #print(edge.id())
+    bad_measurements = list(bad_measurements)
+    return bad_measurements
+
+
+def draw_keypoints(image, keypoints, name='keypoints', delay=0):
+    if image.ndim == 2:
+        image_copy = np.repeat(image[..., np.newaxis], 3, axis=2)
+    else:
+        image_copy = image
+    image_copy = cv2.drawKeypoints(image_copy, keypoints, None, flags=0)
+    cv2.imshow(name, image_copy)
+    cv2.waitKey(delay)
+
+def test_1(args, frame_1, matched_measurements_1, class_id=0, max_iterations=0, gt_pose=None):
+    optimizer = BundleAdjustmentScaleAware(args=args)
+
+    # Poses #
+    optimizer.add_pose(0, frame_1.pose, frame_1.cam, fixed=False) 
+
+    index_point = 0
+    index_general = 0
+    points_start = 6
+    depth_scale_consistenct_start = 10000
+    camera_start = 50000
+
+    # Points frame 1 #
+    for meas in matched_measurements_1:  
+        pt = meas.mappoint
+
+        xy = meas.xy 
+        
+        # 3D Landmark #
+        optimizer.add_point(index_point + points_start, pt.position, fixed=False)
+        optimizer.add_camera_edge(index_general + camera_start, point_id=index_point + points_start, pose_id=0, meas=meas.xy)
+    
+        index_point += 1
+        index_general += 1
+
+    optimizer.optimize(max_iterations)
+
+    print("gt\n")
+    print(gt_pose.matrix())
+
+    print("Before (noise)\n")
+    print("RPE: ", rpe(gt_pose, frame_1.pose))
+    print(frame_1.pose.matrix())
+    print("Scales\n")
+    print(frame_1.scale_aware_frame.scales[class_id], frame_1.scale_aware_frame.scales[class_id])
+
+
+    print(len(matched_measurements_1))
+    print(len(get_bad_measurements(optimizer, args)))
+
+    print("After\n")
+    pose_after_2 = optimizer.get_pose(0)
+    print("RPE: ", rpe(gt_pose, pose_after_2))
+    print(pose_after_2.matrix())     
+
+   
+
+    #scale_1 = optimizer.get_scale(2)
+    #scale_2 = optimizer.get_scale(3)
+
+    #print("Scales\n")
+    #print(scale_1, scale_2)
+
+
+def optimize(args, frame_1, frame_2, matched_measurements_1, matched_measurements_2, class_id=0, max_iterations=0, gt_pose=None):
+    optimizer = BundleAdjustmentScaleAware(args=args)
+
+    # Poses #
+    #optimizer.add_pose(0, frame_1.pose, frame_1.cam, fixed=True)  # offset + 2 * 0 = offset 
+    optimizer.add_pose(1, frame_1.pose, frame_2.cam, fixed=False) # offset + 2 = offset + 2
+
+    """
+    # Scales #
+    optimizer.add_scale(2, frame_1.scale_aware_frame.scales[class_id], fixed=True)
+    optimizer.add_scale(3, frame_2.scale_aware_frame.scales[class_id], fixed=True)
+
+
+    # Scales edges network #
+    information_1 = np.identity(1)
+    information_2 = np.identity(1)
+    if args.use_uncertainties:
+        information_1 *= 1. / frame_1.scale_aware_frame.scales_uncertainty[class_id]
+        information_2 *= 1. / frame_2.scale_aware_frame.scales_uncertainty[class_id]
+    optimizer.add_scale_edge(4, scale_id=2, meas=frame_1.scale_aware_frame.scales[class_id],
+                                  information=information_1)
+    optimizer.add_scale_edge(5, scale_id=3, meas=frame_2.scale_aware_frame.scales[class_id],
+                                  information=information_2)
+    """
+
+    index_point = 0
+    index_general = 0
+    points_start = 6
+    depth_scale_consistenct_start = 10000
+    camera_start = 50000
+
+    # Points frame 1 #
+    for meas in matched_measurements_1:  
+        pt = meas.mappoint
+
+        xy = meas.xy 
+        # 3D Landmark #
+        # offset + 2 * (index_point + 6) + 1
+        # 0: offset + 12 + 1 
+        optimizer.add_point(index_point + points_start, pt.position, fixed=False)
+        #optimizer.add_camera_edge(index_general + camera_start, point_id=index_point + points_start, pose_id=0, meas=meas.xy)
+
+       
+        """ 
+        information = np.identity(1)
+        if args.use_uncertainties:
+            information *= 1. / m.covariance_canonical_measurement
+        optimizer.add_depth_scale_consistency_edge(index_general + depth_scale_consistenct_start, point_id=index_point + points_start, pose_id=0, \
+                                                   scale_id=2, meas=meas.canonical_measurement, information=information)
+        """ 
+        index_general += 1
+        index_point += 1
+    
+    # Frame 2 # 
+    index_point = 0
+    for meas in matched_measurements_2:  
+        pt = meas.mappoint
+
+        optimizer.add_camera_edge(index_general + camera_start, point_id=index_point + points_start, pose_id=1, meas=meas.xy)
+
+        """
+        information = np.identity(1)
+        if args.use_uncertainties:
+            information *= 1. / m.covariance_canonical_measurement
+        optimizer.add_depth_scale_consistency_edge(index_general + depth_scale_consistenct_start, point_id=index_point + points_start, pose_id=1, \
+                                                   scale_id=3, meas=meas.canonical_measurement, information=information)
+        """
+        index_general += 1
+        index_point += 1
+     
+    optimizer.optimize(max_iterations)
+
+    print("gt\n")
+    print(gt_pose.matrix())
+
+    print("Before (noise)\n")
+    print("RPE: ", rpe(gt_pose, frame_2.pose))
+    print(frame_2.pose.matrix())
+    print("Scales\n")
+    print(frame_1.scale_aware_frame.scales[class_id], frame_2.scale_aware_frame.scales[class_id])
+
+
+    print(len(matched_measurements_2))
+    print(len(get_bad_measurements(optimizer, args)))
+
+    print("After\n")
+    pose_after_2 = optimizer.get_pose(1)
+    print("RPE: ", rpe(gt_pose, pose_after_2))
+    print(pose_after_2.matrix())     
+
+   
+
+    #scale_1 = optimizer.get_scale(2)
+    #scale_2 = optimizer.get_scale(3)
+
+    #print("Scales\n")
+    #print(scale_1, scale_2)
+
